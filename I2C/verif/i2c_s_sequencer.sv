47874e83ce16ee0f724a0f400e1602b500386335
Ummidichandrika <92587990+Ummidichandrika@users.noreply.github.com>
2026-05-03 18:13:43 +0530

    Create i2c_s_sequencer.sv

---
+++ I2C/verif/i2c_s_sequencer.sv
@@ -0,0 +1,9 @@
+class i2c_s_sequencer extends uvm_sequencer#(i2c_seq_item);
+  
+  `uvm_component_utils(i2c_s_sequencer)
+  
+  function new(string name="i2c_s_sequencer",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction : new
+  
+endclass : i2c_s_sequencer 
